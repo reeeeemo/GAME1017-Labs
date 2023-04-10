5a9fec145e9010c88b1abc18a3f7fb86ce0eb87f
reeeeemo <robbie@oxley.ca>
2023-04-09 20:05:33 -0400

    [ADD] Lab 9 Finsh

--- GAME1017_LE5_OxleyRobert/GAME1017_Framework/Obstacle.cpp
+++ GAME1017_LE5_OxleyRobert/GAME1017_Framework/Obstacle.cpp
@@ -4,20 +4,47 @@
 Obstacle::Obstacle(const SDL_FRect dst, bool hasImage, const SDL_Rect src, const char* key)
 	:m_hasImage(hasImage), m_pImage(nullptr)
 {
-	// Fill in for lab.
+	m_pos = { dst.x, dst.y };
+	if (m_hasImage)
+	{
+		// Create a new image object and pass the parameters
+		// For you to do for LE5
+	}
 }
 
 Obstacle::~Obstacle()
 {
-	// Fill in for lab.
+	if (m_hasImage)
+	{
+		delete m_pImage;
+		m_pImage = nullptr;
+	}
 }
 
 void Obstacle::Update()
 {
-	// Fill in for lab.
+	m_pos.x -= kScrollSpeed;
+	if (m_hasImage)
+	{
+		// Update the image dst rect.
+		//m_pImage->GetDst()->x;
+	}
 }
 
 void Obstacle::Render()
 {
-	// Fill in for lab.
+	// Take out when using image.
+	SDL_FRect m_dst = { m_pos.x, m_pos.y, 128, 128 };
+	if (m_hasImage)
+	{
+		// Render image
+		// SDL_RenderCopyF(...);
+		// Red Box for Week 9 lab..
+		SDL_SetRenderDrawColor(REMA::GetRenderer(), 255, 0, 0, 255);
+		SDL_RenderFillRectF(REMA::GetRenderer(), &m_dst);
+	}
+	// Render border for just week 9 lab.
+	
+	SDL_SetRenderDrawColor(REMA::GetRenderer(), 128, 0, 128, 255);
+	SDL_RenderDrawRectF(REMA::GetRenderer(), &m_dst);
 }
--- GAME1017_LE5_OxleyRobert/GAME1017_Framework/ObstacleRow.cpp
+++ GAME1017_LE5_OxleyRobert/GAME1017_Framework/ObstacleRow.cpp
@@ -6,11 +6,45 @@ ObstacleRow::ObstacleRow():m_gapCtr(0), m_gapMax(3)
 	// Create the vector now.
 	// 
 	// Fill in for lab.
+	for (int i = 0; i < 9; i++)
+	{
+		m_obstacles.push_back(new Obstacle( { 128.0f * i, 384.0f, 128.0f, 128.0f } ));
+	}
+	m_obstacles.shrink_to_fit();
 }
 
 void ObstacleRow::Update()
 {
-	// Fill in for lab.
+
+	
+
+	for (const auto obstacle : m_obstacles)
+	{
+		// When the first obstacle goes completely offscreen
+		if (obstacle != nullptr) // Error checker!
+		{
+			if (obstacle->GetPos().x <= 0 - 128.0f)
+			{
+				delete m_obstacles[0];
+				m_obstacles[0] = nullptr;
+				m_obstacles.erase(m_obstacles.begin());
+
+				if (m_gapCtr++ % m_gapMax == 0)
+				{
+					m_obstacles.push_back(new Obstacle({ m_obstacles.back()->GetPos().x + 128.0f, 384.0f, 128.0f, 128.0f }, true));
+				}
+				else {
+					m_obstacles.push_back(new Obstacle({ m_obstacles.back()->GetPos().x + 128.0f, 384.0f, 128.0f, 128.0f}));
+				}
+
+
+				m_obstacles.shrink_to_fit();
+			}
+			else { // Scroll the obstacles.
+				obstacle->Update();
+			}
+		}
+	}
 }
 
 void ObstacleRow::Render()
@@ -19,6 +53,11 @@ void ObstacleRow::Render()
 	SDL_SetRenderDrawColor(REMA::GetRenderer(), 0, 0, 255, 255);
 	SDL_RenderDrawLine(REMA::GetRenderer(), 0, 512, 1024, 512);
 	// Render the obstacles.
-	// 
-	// Fill in for lab.
+	for (const auto obstacle : m_obstacles)
+	{
+		if (obstacle != nullptr) // Error checker!
+		{
+			obstacle->Render();
+		}
+	}
 }
